37c9c30ab58ff902c8dcc6aaeb2753310df3539c
toydogcat <toydogcat@gmail.com>
2026-04-06 12:44:57 +0800

    refactor: update socket.io-client import to use default export

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // @ts-ignore
-import { io } from 'socket.io-client';
+import io from 'socket.io-client';
 
 const BASE_URL = import.meta.env.VITE_API_URL || window.location.origin;
 const API_BASE = BASE_URL + '/api';
