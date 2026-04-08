8efdfc80c9960c68e9d49692e4faee131bd851d3
toydogcat <toydogcat@gmail.com>
2026-04-08 11:21:16 +0800

    feat: import watch from vue in ObsidianView.vue

--- src/views/ObsidianView.vue
+++ src/views/ObsidianView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 import { apiService } from '../services/api';
 import { marked } from 'marked';
 
